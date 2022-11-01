21bca17f3b073268052f0577b7d10921ab8d5259
iamalexneumann <iamalexneumann@yandex.ru>
2022-11-01 19:47:11 +0300

    Добавляет базовые шаблоны

--- local/templates/komandae/header.php
+++ local/templates/komandae/header.php
@@ -1,17 +1,111 @@
-<?
-if(!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true)
-	die();
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
+/**
+ * @var CMain $APPLICATION
+ * @var CMain $CurDir
+ * @var CSite $arSite
+ * @var COption $siteparam_scripts_head
+ * @var COption $siteparam_scripts_body_before
+ * @var COption $siteparam_main_logo
+ * @var COption $siteparam_main_phone
+ * @var COption $siteparam_main_phone_tel
+ */
+use Bitrix\Main\Localization\Loc;
+Loc::loadLanguageFile(__FILE__);
 ?>
 <!DOCTYPE html>
-<html>
-	<head>
-		<?$APPLICATION->ShowHead();?>
-		<title><?$APPLICATION->ShowTitle();?></title>
-		<link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" /> 	
-	</head>
-	<body>
-		<div id="panel">
-			<?$APPLICATION->ShowPanel();?>
-		</div>
-	
-						
+<html lang="<?= LANGUAGE_ID; ?>">
+<head>
+    <title><?php $APPLICATION->ShowTitle(); ?></title>
+    <?php
+    echo $siteparam_scripts_head;
+    use Bitrix\Main\UI\Extension;
+    use Bitrix\Main\Page\Asset;
+    Extension::load(
+        [
+            'ui.bootstrap5',
+            'ui.fonts.font-awesome',
+            'ui.fonts.montserrat',
+            'ui.lazysizes',
+        ]
+    );
+    Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . '/main.js');
+    $APPLICATION->ShowHead();
+    ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no, maximum-scale=1">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= SITE_TEMPLATE_PATH; ?>/img/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= SITE_TEMPLATE_PATH; ?>/img/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= SITE_TEMPLATE_PATH; ?>/img/favicons/favicon-16x16.png">
+    <link rel="manifest" href="<?= SITE_TEMPLATE_PATH; ?>/img/favicons/site.webmanifest">
+    <link rel="mask-icon" href="<?= SITE_TEMPLATE_PATH; ?>/img/favicons/safari-pinned-tab.svg" color="#2a2b87">
+    <link rel="shortcut icon" href="<?= SITE_TEMPLATE_PATH; ?>/img/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#2a2b87">
+    <meta name="msapplication-config" content="<?= SITE_TEMPLATE_PATH; ?>/img/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#2a2b87">
+</head>
+<body>
+    <?= $siteparam_scripts_body_before; ?>
+    <?php $APPLICATION->ShowPanel(); ?>
+    <header class="main-header">
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand"
+                   title="<?= htmlspecialchars($arSite['SITE_NAME']); ?>"
+                    <?php if ($CurDir !== '/'): ?> href="/"<?php endif; ?>>
+                    <img src="<?= $siteparam_main_logo; ?>" width="75" height="75" alt="<?= htmlspecialchars($arSite['SITE_NAME']); ?>">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" aria-expanded="false"
+                        data-bs-target="#mainNavbar" aria-controls="mainNavbar" aria-label="<?= Loc::getMessage('HEADER_NAVBAR_ARIA_LABEL'); ?>">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="mainNavbar">
+                    <div class="ms-auto me-auto">
+                        <?php
+                        $APPLICATION->IncludeComponent(
+                            "bitrix:menu",
+                            "main_menu",
+                            array(
+                                "ALLOW_MULTI_SELECT" => "N",
+                                "CHILD_MENU_TYPE" => "main_submenu",
+                                "COMPOSITE_FRAME_MODE" => "A",
+                                "COMPOSITE_FRAME_TYPE" => "AUTO",
+                                "DELAY" => "N",
+                                "MAX_LEVEL" => "2",
+                                "MENU_CACHE_GET_VARS" => array(
+                                ),
+                                "MENU_CACHE_TIME" => "3600",
+                                "MENU_CACHE_TYPE" => "A",
+                                "MENU_CACHE_USE_GROUPS" => "Y",
+                                "ROOT_MENU_TYPE" => "main_menu",
+                                "USE_EXT" => "Y",
+                                "COMPONENT_TEMPLATE" => "main_menu"
+                            ),
+                            false
+                        ); ?>
+                    </div>
+                    <div class="d-flex align-items-center">
+                        <a href="tel:<?= $siteparam_main_phone_tel; ?>" title="<?= Loc::getMessage('HEADER_MAIN_PHONE_TITLE'); ?>"><?= $siteparam_main_phone; ?></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container">
+            <header>
+                <?php
+                $APPLICATION->IncludeComponent(
+                    "bitrix:breadcrumb",
+                    "breadcrumb",
+                    Array(
+                        "START_FROM" => "0",
+                        "PATH" => "",
+                        "SITE_ID" => SITE_ID,
+                    ),
+                    false
+                ); ?>
+                <h1><?php $APPLICATION->ShowTitle(false); ?></h1>
+                <?php $APPLICATION->ShowViewContent('MAIN_SUBTITLE'); ?>
+            </header>
