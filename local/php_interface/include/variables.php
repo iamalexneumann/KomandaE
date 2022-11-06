cacea0f9214dd7cb6c664190aac4446c5f3940f6
iamalexneumann <iamalexneumann@yandex.ru>
2022-11-06 16:36:37 +0300

    Добавляет переменные проекта и функции в init.php

--- local/php_interface/include/variables.php
+++ local/php_interface/include/variables.php
@@ -4,15 +4,24 @@
  */
 use \Bitrix\Main\Config\Option;
 use \Bitrix\Main\SiteTable;
-$siteparam_business_entity = Option::get('askaron.settings', 'UF_BUSINESS_ENTITY', '');
+
 $siteparam_main_logo = CFile::GetPath(Option::get('askaron.settings', 'UF_MAIN_LOGO', ''));
+$siteparam_footer_logo = CFile::GetPath(Option::get('askaron.settings', 'UF_FOOTER_LOGO', ''));
+$siteparam_logo_name = Option::get('askaron.settings', 'UF_LOGO_NAME', '');
+$siteparam_logo_description = Option::get('askaron.settings', 'UF_LOGO_DESCRIPTION', '');
 $siteparam_main_phone = Option::get('askaron.settings', 'UF_MAIN_PHONE', '');
-$siteparam_whatsapp_number = Option::get('askaron.settings', 'UF_WHATSAPP_NUMBER', '');
-//$siteparam_whatsapp_message = transform_text_to_whatsapp_link(Option::get('askaron.settings', 'UF_WHATSAPP_MESSAGE', ''));
-$siteparam_telegram_link = Option::get('askaron.settings', 'UF_TELEGRAM_LINK', '');
-$siteparam_main_email = Option::get('askaron.settings', 'UF_MAIN_EMAIL', '');
-$siteparam_main_address = Option::get('askaron.settings', 'UF_MAIN_ADDRESS', '');
+$siteparam_email = Option::get('askaron.settings', 'UF_EMAIL', '');
 $siteparam_schedule = Option::get('askaron.settings', 'UF_SCHEDULE', '');
+$siteparam_whatsapp_number = Option::get('askaron.settings', 'UF_WHATSAPP_NUMBER', '');
+$siteparam_whatsapp_message = transform_text_to_whatsapp_link(Option::get('askaron.settings', 'UF_WHATSAPP_MESSAGE', ''));
+$siteparam_telegram_link = Option::get('askaron.settings', 'UF_TELEGRAM_LINK', '');
+$siteparam_address = Option::get('askaron.settings', 'UF_ADDRESS', '');
+
+$siteparam_price_aerial_platform = Option::get('askaron.settings', 'UF_PRICE_AERIAL_PLATFORM', '');
+$siteparam_price_truck_crane = Option::get('askaron.settings', 'UF_PRICE_TRUCK_CRANE', '');
+$siteparam_price_manipulators = Option::get('askaron.settings', 'UF_PRICE_MANIPULATORS', '');
+$siteparam_photos = Option::get('askaron.settings', 'UF_PHOTOS', '');
+
 $siteparam_api_key_yandex_map = Option::get('askaron.settings', 'UF_API_KEY_YANDEX_MAP', '');
 $siteparam_coors_yandex_map = Option::get('askaron.settings', 'UF_COORS_YANDEX_MAP', '');
 $siteparam_zoom_yandex_map = Option::get('askaron.settings', 'UF_ZOOM_YANDEX_MAP', '');
@@ -21,8 +30,8 @@ $siteparam_scripts_head = Option::get('askaron.settings', 'UF_SCRIPTS_HEAD', '')
 $siteparam_scripts_body_before = Option::get('askaron.settings', 'UF_SCRIPTS_BODY_BEFORE', '');
 $siteparam_scripts_body_after = Option::get('askaron.settings', 'UF_SCRIPTS_BODY_AFTER', '');
 
-//$siteparam_main_phone_tel = get_tel_from_phone($siteparam_main_phone);
-//$siteparam_whatsapp_number_tel = get_tel_from_phone($siteparam_whatsapp_number);
+$siteparam_main_phone_tel = get_tel_from_phone($siteparam_main_phone);
+$siteparam_whatsapp_number_tel = get_tel_from_phone($siteparam_whatsapp_number);
 
 $CurDir = $APPLICATION->GetCurDir();
 $CurUri = $APPLICATION->GetCurUri();
